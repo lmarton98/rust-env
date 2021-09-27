fab12e4b42dead37a5e16586f179ba4d3bd16dd4
László Márton <lmarton98@gmail.com>
2021-09-27 21:43:40 +0000

    Add Fibonacci code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn fibo(n: u32) -> u32 {
+    if n <= 1 {
+        1_u32
+    } else {
+        fibo(n - 1) + fibo(n - 2)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, fibo");
+    for i in 1..12 {
+        let fib = fibo(i);
+        println!("N = {:?}, fibo(N) = {:?}", i, fib);
+    }
 }
